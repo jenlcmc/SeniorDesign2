5da56e9120e0f7dafb0f2438253ac5fa8c6a5cc7
Ren Matsubara <43124584+renm10@users.noreply.github.com>
2024-02-24 09:34:12 -0800

    Refactored code snippet using chatGPT suggestions

--- chatGPT/RenMatsubara_getValidGuess.cpp
+++ chatGPT/RenMatsubara_getValidGuess.cpp
@@ -2,43 +2,48 @@
 
 using namespace std;
 
-void getValidGuess(char&, vector<char>, string, int, string);
+void getValidGuess(char&, vector<char>, string, int);
 
 //Ask the user and get a valid guess
-void getValidGuess(char &userguess, vector<char> userguesses, string guess, int miss, string errMsg)
+void getValidGuess(char &userguess, vector<char> userguesses, string guess, int miss)
 {
     string guessedString;
+    string errMsg;
 
-    clearScreen();
-    printMan(miss);
-    cout << endl;
-    cout << guess << "   " << errMsg << endl;
-    errMsg = ""; //Reset the Error State
-
-    cout << "Enter Guess [a ~ z]: ";
-    cin >> guessedString;
-    cout << endl;
-
-    if (!isOneCharLong(guessedString)) //Check if the input is one character long
+    while (true)
     {
-        errMsg = "Guess has to be one character long!";
-        getValidGuess(userguess, userguesses, guess, miss, errMsg);
-    }
-    else
-    {
-        userguess = guessedString[0];
-        if (!isalpha(userguess)) //Check if it is an alphabet
-        {   
-            errMsg = "Guess has to be an alphabet!";
-            getValidGuess(userguess, userguesses, guess, miss, errMsg);
+        clearScreen();
+        printMan(miss);
+        cout << endl;
+        cout << guess << "   " << errMsg << endl;
+        errMsg = ""; // Reset the Error State
+
+        cout << "Enter Guess [a ~ z]: ";
+        cin >> guessedString;
+        cout << endl;
+
+        if (!isOneCharLong(guessedString)) // Check if the input is one character long
+        {
+            errMsg = "Guess has to be one character long!";
         }
         else
         {
-            if (guessedAlready(userguess, userguesses)) //Check if the guess hasn't been guessed already
+            userguess = guessedString[0];
+            if (!isalpha(userguess)) // Check if it is an alphabet
             {
-                errMsg += userguess;
-                errMsg += " has been guessed already";
-                getValidGuess(userguess, userguesses, guess, miss, errMsg);
+                errMsg = "Guess has to be an alphabet!";
+            }
+            else
+            {
+                if (guessedAlready(userguess, userguesses)) // Check if the guess hasn't been guessed already
+                {
+                    errMsg = userguess;
+                    errMsg += " has been guessed already";
+                }
+                else
+                {
+                    break; // Break the loop if the input is valid
+                }
             }
         }
     }
