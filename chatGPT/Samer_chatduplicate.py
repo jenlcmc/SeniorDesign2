41da828822af717849d9bfb9aced5192b0f2d773
SY <94941799+FPSwitch@users.noreply.github.com>
2024-02-25 10:52:57 -0800

    Documentation for Task 2.1 (Issue 100)
    Here is my code that references the concerns I had from Issue #100

---
+++ chatGPT/Samer_chatduplicate.py
@@ -0,0 +1,12 @@
+# ChatGPT Assignment Task 2.1
+
+# Original Code
+def multiply(num1, num2):   #Function to return product
+    prod = num1 * num2
+    return prod
+
+def percentage(sample, total):  #percentage of a fraction
+    div = sample/total
+    percent = div*100
+    return percent
+
