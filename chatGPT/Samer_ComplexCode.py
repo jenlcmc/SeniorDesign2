105e0293554b2b3ffcc0b633e27cfe58ce86e40a
SY <94941799+FPSwitch@users.noreply.github.com>
2024-02-26 01:26:08 -0800

    Update Samer_ComplexCode.py
    Forgot to add issue code

---
+++ chatGPT/Samer_ComplexCode.py
@@ -0,0 +1,8 @@
+# Python program for Complex Code of Percentiles
+
+def percentile(pth, total):
+    """
+    This function returns the position of a value based on the percentile
+    and total number of values in the data
+    """
+    return (pth/100) * total
